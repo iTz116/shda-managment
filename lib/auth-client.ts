f5cfb3a1c6eeb86e5c9a618170519f48d940e2d9
xBit <itzkls911@gmail.com>
2025-11-11 18:37:43 +0300

    fix auth

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -5,7 +5,7 @@ import { ac, admin, user } from "@/lib/permissions"
 
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000",
+    baseURL: process.env.NEXT_PUBLIC_BASE_URL,
     plugins: [
       adminClient({
         ac,
